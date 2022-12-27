fef14fde5e503a9d7f21357ce2401480e5b02131
hanhnd <hanhnd156@gmail.com>
2022-12-27 17:06:06 +0700

    add banner

--- src/pages/home/components/Banner.vue
+++ src/pages/home/components/Banner.vue
@@ -1,13 +1,17 @@
 <template>
-    <div class="banner-wrapper">BANNER</div>
+    <div class="banner-wrapper">
+        <img
+            src="https://images.gr-assets.com/misc/1619034579-1619034579_goodreads_misc.png"
+        />
+    </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style lang="scss" scoped>
-.banner-wrapper {
+.banner-wrapper img {
     width: 100%;
-    height: 100px;
-    background: red;
+    height: 300px;
+    object-fit: fill;
 }
 </style>
